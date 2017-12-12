902ca7be37597bc39dd58210ba203a59aa4531b1
M4hler <oskar.makos.makowski@gmail.com>
2017-12-12 14:57:41 +0100

    Server Hello, World; Test commit after moving to IntelliJ

--- ChineseCheckers/src/main/java/server/Server.java
+++ ChineseCheckers/src/main/java/server/Server.java
@@ -2,5 +2,10 @@ package server;
 
 public class Server {
     public Game startGame() {return null;} //game parameters as arguments (like nb of players, size of board etc)
+
+    public static void main(String[] args)
+    {
+        System.out.println("Server Hello, World!");
+    }
     
 }
